0940d7431f03ffe7101de58c2fec9a170b36ca1a
subramanyanavada123 <57627587+subramanyanavada123@users.noreply.github.com>
2022-01-21 22:46:15 +0530

    Create login.jsx

---
+++ login.jsx
@@ -0,0 +1,55 @@
+;<>
+  <meta charSet="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
+  />
+  <title>code-ision</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
+  />
+  <link rel="stylesheet" href="assets/fonts/ionicons.min.css" />
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/css/pikaday.min.css"
+  />
+  <link rel="stylesheet" href="assets/css/Login-Form-Clean.css" />
+  <section />
+  <section className="login-clean">
+    <form method="post">
+      <h2 className="visually-hidden">Login Form</h2>
+      <div className="illustration">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="bi bi-wallet2"
+          style={{ color: "var(--bs-blue)" }}
+        >
+          <path
+            fillRule="evenodd"
+            d="M12.136.326A1.5 1.5 0 0 1 14 1.78V3h.5A1.5 1.5 0 0 1 16 4.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 13.5v-9a1.5 1.5 0 0 1 1.432-1.499L12.136.326zM5.562 3H13V1.78a.5.5 0 0 0-.621-.484L5.562 3zM1.5 4a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"
+          />
+        </svg>
+      </div>
+      <div className="mb-3" />
+      <div className="mb-3" />
+      <div className="mb-3">
+        <button
+          className="btn btn-primary d-block w-100"
+          type="submit"
+          style={{ background: "var(--bs-blue)" }}
+        >
+          Connect to wallet
+        </button>
+      </div>
+      <a className="forgot" href="#">
+        New to community?
+      </a>
+    </form>
+  </section>
+</>
